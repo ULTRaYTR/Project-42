54e7c29cc9a8820b6f8adac36ae180760863db9a
ULTRaYTR <76477523+ULTRaYTR@users.noreply.github.com>
2022-04-03 18:34:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,9 +29,7 @@ function draw() {
   if(!hasDocked){
 
      spacecraft = spacecraft.x + random(-1,1);
-     spacecraft.x = spacecraft.x + Random(-1,1);
-     spacecraft.x = spacecraft.x + randomNumbers(-1,1);
-     spacecraft.x = spacecraft.x + random(-1,1);
+  
     
   if(keyDown("UP_ARROW")){
     spacecraft.y = spacecraft.y -2;
@@ -52,19 +50,7 @@ function draw() {
   }
 }
 
-   if(spacecraft.y <= (iss.y+180) && spacecraft.x <= (iss.x-10)){
-     hasDocked = true;
-     textSize(25);
-     fill("white")
-     text("Docking Successful!", 200, 300);
-   }
 
-   if(spacecraft.y <= (iss.y-70) && spacecraft.x <= (iss.x-10)){
-     hasDocked = true;
-     textSize(25);
-     fill("white")
-     text("Docking Successful!", 200, 300);
-   }
 
    if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
      hasDocked = true;
@@ -73,13 +59,7 @@ function draw() {
      text("Docking UnSuccessful!", 200, 300);
    }
 
-   if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
-     hasDocked = true;
-     textSize(25);
-     fill("white")
-     text("Docking Successful!", 200, 300);
-   }
-
+   
   drawSprites();
 }
 
